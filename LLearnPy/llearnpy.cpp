b7dcd0fe1d28d1273f876c81c317e15cfe976125
lapis <klimantovartem@gmail.com>
2023-04-11 19:09:13 +0300

    button pos 0.1

--- LLearnPy/llearnpy.cpp
+++ LLearnPy/llearnpy.cpp
@@ -13,8 +13,24 @@ LLearnPy::~LLearnPy()
     delete ui;
 }
 
-void LLearnPy::setInterfaceStyle()
-{
+void LLearnPy::resizeEvent(QResizeEvent *event){
+    Q_UNUSED(event);
+    LLearnPy::updateButtonPosition();
+}
+
+void LLearnPy::updateButtonPosition(){
+    QRect graphicsViewGeometry = ui->centralwidget->geometry();
+    ui->aboutButton->setGeometry({20, graphicsViewGeometry.height() - ui->aboutButton->geometry().height() - 20,
+                                  ui->aboutButton->geometry().width(), ui->aboutButton->geometry().height()});
+
+    ui->LectureButton->setGeometry({});
+
+    ui->TaskButton->setGeometry({});
+
+    ui->SandboxButton->setGeometry({});
+}
+
+void LLearnPy::setInterfaceStyle(){
     QFontDatabase::addApplicationFont(":/res/font/Lumberjack.otf");
     ui->MainLabel->setStyleSheet("color: rgb(171, 139, 56)");
     this->setStyleSheet(WindowStyleDesign::getMainWindowStyle());
